f2a30f485b8393679f448a515486a248253db915
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2017-09-18 17:27:35 +0200

    SAI-208 renamed index and blocked unknown fields

--- create-index.sh
+++ create-index.sh
@@ -6,7 +6,7 @@
 # run: npm install -g js-yaml@3.8.3
 
 HOST=${HOST:-http://localhost:9200}
-INDEX=${1:-datacite}
+INDEX=${1:-gerdi}
 
 if [ -n "$FORCE" ]; then
     echo "Trying to delete old index!"
