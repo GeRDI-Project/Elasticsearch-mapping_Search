e24515702a413bb75e76b4bf40720e26b4b86af6
Nelson Tavares de Sousa <ntd@informatik.uni-kiel.de>
2018-07-10 14:53:39 +0200

    SAI-1008 added comments

--- job.sh
+++ job.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+# Copyright © 2018 Nelson Tavares de Sousa (http://www.gerdi-project.de/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # treat unset variables as an error when substituting
 set -u
 
@@ -10,7 +24,7 @@ echo "Checking if an old index exists in order to replace it"
 
 CreateNewIndex() {
   TARGETINDEX=$ALIAS-$(date +"%d%m%y-%H%M")
-  js-yaml metadata-index-settings.yml | cur -s -XPUT "$HOST/$TARGETINDEX?format=yaml" -d @- --header "Content-Type: application/json"
+  js-yaml metadata-index-settings.yml | curl -s -XPUT "$HOST/$TARGETINDEX?format=yaml" -d @- --header "Content-Type: application/json"
   curl -s -XPOST "$HOST/_aliases?format=yaml" -d '{
     "actions": [
     { "add": { "index": "'$TARGETINDEX'", "alias": "'$ALIAS'" } }
@@ -20,7 +34,7 @@ CreateNewIndex() {
 }
 
 UpdateIndex() {
-  SOURCEINDEX=$(curl -s "$HOST/$ALIAS" | sed -nE 's/.*"provided_name":"(.*)"\,"creation_date.*/\1/p')
+  SOURCEINDEX=$(curl -s "$HOST/$ALIAS" | sed -nE 's/.*"provided_name":"(.*)"\,"creation_date.*/\1/p') # curl returns a document describing the concrete index, or a missing index exception
   TARGETINDEX=$ALIAS-$(date +"%d%m%y-%H%M")
   js-yaml metadata-index-settings.yml | curl -s -XPUT "$HOST/$TARGETINDEX?format=yaml" -d @- --header "Content-Type: application/json"
   curl -s -XPOST "$HOST/_reindex?format=yaml" -d '{
@@ -41,7 +55,7 @@ UpdateIndex() {
 }
 
 # Update index on start
-SOURCEINDEX=$(curl -s "$HOST/$ALIAS" | sed -nE 's/.*"provided_name":"(.*)"\,"creation_date.*/\1/p')
+SOURCEINDEX=$(curl -s "$HOST/$ALIAS" | sed -nE 's/.*"provided_name":"(.*)"\,"creation_date.*/\1/p') # curl returns a document describing the concrete index, or a missing index exception
 if [ "$SOURCEINDEX" != "" ]; then
   echo "Updating to a new schema"
   UpdateIndex
